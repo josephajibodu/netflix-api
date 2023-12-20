521dc360e2d30b126b3b61e5189def2491096c3f
cremirdevio <josephajibodu@gmail.com>
2023-12-20 12:18:53 +0100

    validation done

--- src/routes/admin/movies.route.js
+++ src/routes/admin/movies.route.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { ObjectId } = require("mongodb");
-const Database = require("../../database/db");
 const MovieController = require("../../controllers/admin/movie.controller");
+const validator = require("../../validators");
 
-// Movie Schema
-// - title
-// - description
-// - release_year
-// - genres
-// - duration
-// - casts (list of users)
-// - director (a user)
 
-router.get("/",  MovieController.index);
+router.get("/", MovieController.index);
 
 router.get("/:id", MovieController.show);
 
-router.post("/", MovieController.create);
+router.post("/", validator.createMovieValidation(), validator.validate(), MovieController.create);
 
 router.put("/:id", MovieController.update);
 
